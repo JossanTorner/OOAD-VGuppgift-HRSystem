5e6cb6fb09514f9ff02c2986bffb43dcf2e0e134
Josefin Törner <josefintorner@gmail.com>
2024-12-13 22:33:58 +0100

    something

--- src/HR/Commands/NewEmployeeCommand.java
+++ src/HR/Commands/NewEmployeeCommand.java
@@ -5,21 +5,21 @@ import java.util.HashMap;
 
 public class NewEmployeeCommand implements Command{
 
-    HashMap<Long, Employee> employees;
+    HashMap<Long, Employee> employeeList;
     Employee newEmployee;
 
     public NewEmployeeCommand(HashMap<Long, Employee> employeeList, Employee newEmployee) {
-        this.employees = employeeList;
+        this.employeeList = employeeList;
         this.newEmployee = newEmployee;
     }
 
     @Override
     public void execute() {
-        employees.put(newEmployee.getEmployeeId(), newEmployee);
+        employeeList.put(newEmployee.getEmployeeId(), newEmployee);
     }
 
     @Override
     public void undo() {
-        employees.remove(newEmployee.getEmployeeId());
+        employeeList.remove(newEmployee.getEmployeeId());
     }
 }
